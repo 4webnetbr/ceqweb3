3af0e21b87ac1166482e5eeba2d105272014d40f
Douglas Ferreira <douglas@4web.net.br>
2025-09-18 23:50:15 -0300

    Atualizacao 18/09/2025 23:50:14

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,281 +2,150 @@
 
 namespace Config;
 
-use Estoque\Origem;
-use Estoque\Familia;
-use Produto\Produto;
-use Produto\ProClasse;
-use Produto\Fabricante;
-use App\Controllers\Home;
-use App\Controllers\Utils;
-use Produto\ProIngrediente;
-use App\Controllers\Showfile;
-use App\Controllers\CfgEmpresa;
-use App\Controllers\WorkAnalise;
-use App\Controllers\Ws\WsCeqweb;
 use CodeIgniter\Config\Services;
-use App\Controllers\CriaEtiqueta;
-use App\Controllers\Produto\Lote;
-use App\Controllers\Config\CfgCor;
-use App\Controllers\Config\CfgMenu;
-use App\Controllers\Config\CfgTela;
-use App\Controllers\Analise\Analise;
-use App\Controllers\CriaEtiquetaZPL;
-use App\Controllers\Config\CfgModulo;
-use App\Controllers\Config\CfgPerfil;
-use App\Controllers\Config\CfgStatus;
-use App\Controllers\Estoque\Deposito;
-use App\Controllers\Config\CfgFuncoes;
-use App\Controllers\Config\CfgUsuario;
-use App\Controllers\Estoque\Movimento;
-use App\Controllers\Estoque\Transacao;
-use App\Controllers\Config\CfgMensagem;
-use App\Controllers\Config\Home_config;
-use App\Controllers\Estoque\Requisicao;
-use App\Controllers\Estoque\CfgEtiqueta;
-use App\Controllers\Estoque\OcoTipoAcao;
-use App\Controllers\Micro\AnaRequisicao;
-use App\Controllers\Config\CfgDicionario;
-use App\Controllers\Estoque\SaldoEstoque;
-use App\Controllers\Estoque\AteRequisicao;
-use App\Controllers\Estoque\CfgLayoutEtiq;
-use App\Controllers\Estoque\EtqProdutoReq;
-use App\Controllers\Estoque\ConfRequisicao;
-use App\Controllers\Estoque\TipoMovimentacao;
-use App\Controllers\Estoque\OcoTipoOcorrencia;
+use CodeIgniter\Router\RouteCollection;
 
-// Create a new instance of our RouteCollection class.
+/**
+ * @var RouteCollection $routes
+ */
 $routes = Services::routes();
 
-/*
- * --------------------------------------------------------------------
- * Router Config
- * --------------------------------------------------------------------
- */
-$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultNamespace('App\\Controllers');
 $routes->setDefaultController('login');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(true);
 $routes->set404Override(function () {
     log_message('critical', 'Rota 404 chamada: {uri}', ['uri' => current_url()]);
-    $dadosTela = [
+    return view('vw_semacesso', [
         'title' => current_url(),
         'permissao' => false,
         'erromsg' => "<h2>Atenção</h2>O Caminho <b>" . current_url() . "</b><br>
-            <span style='color:red; font-size:16px'>Não foi Encontrado!</span><br>
-            Informe o Problema ao Administrador do Sistema!",
-    ];
+        <span style='color:red; font-size:16px'>Não foi Encontrado!</span><br>
+        Informe o Problema ao Administrador do Sistema!",
+    ]);
+});
+$routes->setAutoRoute(false);
 
-    // Renderiza a view diretamente
-    return view('vw_semacesso', $dadosTela);
+// Rotas Padrão
+$routes->get('/', 'login::index', ['as' => 'login_index']);
+$routes->get('home_config', 'Config\\Home_config::index', ['as' => 'home_config_index']);
+$routes->get('WorkAnalise', 'WorkAnalise::index', ['as' => 'workanalise_index']);
+$routes->get('teste-handler', 'Home::testarEmailHandler', ['as' => 'home_testarEmailHandler']);
+$routes->get('teste-email', 'Home::testeEmailManual', ['as' => 'home_testeEmailManual']);
+
+$routes->match(['get', 'post'], 'Notifica/(:any)', 'Notifica::$1', ['as' => 'notifica_verNotifica_match']);
+// Grupo: Utils
+$routes->group('Utils', static function ($routes) {
+    $routes->get('/', 'Utils::index', ['as' => 'utils_index']);
+    $routes->match(['get', 'post'], '(:any)', 'Utils::$1', ['as' => 'utils_match']);
 });
 
-$routes->setAutoRoute(true);
+// Grupo: Showfile
+$routes->group('Showfile', static function ($routes) {
+    $routes->get('/', 'Showfile::show', ['as' => 'showfile_show']);
+    $routes->match(['get', 'post'], '(:any)', 'Showfile::show/$1', ['as' => 'showfile_show_match']);
+});
 
-// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
-// where controller filters or CSRF protection are bypassed.
-// If you don't want to define all routes, please use the Auto Routing (Improved).
-// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-// $routes->setAutoRoute(false);
+// Grupo: CriaEtiqueta
+$routes->group('CriaEtiqueta', static function ($routes) {
+    $routes->get('/', 'CriaEtiqueta::emiteEtiqueta', ['as' => 'criaetiqueta_emiteEtiqueta']);
+    $routes->match(['get', 'post'], 'emiteEtiqueta/(:any)', 'CriaEtiqueta::emiteEtiqueta/$1', ['as' => 'criaetiqueta_emiteEtiqueta_match']);
+    $routes->match(['get', 'post'], 'previewEtiquetaViaAjax/(:any)', 'CriaEtiqueta::previewEtiquetaViaAjax/$1', ['as' => 'criaetiqueta_previewEtiquetaViaAjax_match']);
+});
 
-/*
- * --------------------------------------------------------------------
- * Route Definitions
- * --------------------------------------------------------------------
- */
+// Grupo: CriaEtiquetaZPL
+$routes->group('CriaEtiquetaZPL', static function ($routes) {
+    $routes->get('/', 'CriaEtiquetaZPL::emiteEtiqueta', ['as' => 'criaetiquetazpl_emiteEtiqueta']);
+    $routes->match(['get', 'post'], 'emiteEtiqueta/(:any)', 'CriaEtiquetaZPL::emiteEtiqueta/$1', ['as' => 'criaetiquetazpl_emiteEtiqueta_match1']);
+    $routes->match(['get', 'post'], 'emiteEtiqueta/(:any)/(:any)', 'CriaEtiquetaZPL::emiteEtiqueta/$1/$2', ['as' => 'criaetiquetazpl_emiteEtiqueta_match2']);
+    $routes->match(['get', 'post'], 'previewEtiquetaViaAjax/(:any)', 'CriaEtiquetaZPL::previewZPL/$1', ['as' => 'criaetiquetazpl_previewZPL_match']);
+});
 
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
-$routes->get('/', 'login::index');
-$routes->get('/home_config', 'Config\Home_config::index');
-$routes->get('WorkAnalise', 'WorkAnalise::index');
+// Controladores de Configuração
+$cfgControllers = [
+    'CfgCor', 'CfgModulo', 'CfgTela', 'CfgMenu', 'CfgDicionario',
+    'CfgFuncoes', 'CfgPerfil', 'CfgUsuario', 'CfgMensagem',
+    'CfgStatus', 'CfgLayoutEtiq', 'CfgEtiqueta', 'CfgEmpresa', 'CfgImpressora'
+];
 
-$routes->get('/Utils', 'Utils::index');
-$routes->get('/Utils/(:any)', 'Utils::$1');
-$routes->post('/Utils/(:any)', 'Utils::$1');
+foreach ($cfgControllers as $ctrl) {
+    $routes->group($ctrl, static function ($routes) use ($ctrl) {
+        $name = strtolower($ctrl);
+        $routes->get('/', "Config\\$ctrl::index", ['as' => "{$name}_index"]);
+        $routes->match(['get', 'post'], '(:any)', "Config\\$ctrl::$1", ['as' => "{$name}_match"]);
+    });
+}
 
-$routes->get('/Showfile', 'Showfile::show');
-$routes->get('/Showfile/(:any)', 'Showfile::show/$1');
-$routes->post('/Showfile/(:any)', 'Showfile::show/$1');
+// Rotas especiais para CfgModulo
+$routes->group('CfgModulo', static function ($routes) {
+    $routes->get('000', 'Config\\CfgModulo::add', ['as' => 'cfgmodulo_add']);
+    $routes->get('100/(:any)', 'Config\\CfgModulo::show/$1', ['as' => 'cfgmodulo_show_match']);
+    $routes->get('200/(:any)', 'Config\\CfgModulo::edit/$1', ['as' => 'cfgmodulo_edit_match']);
+    $routes->get('300/(:any)', 'Config\\CfgModulo::delete/$1', ['as' => 'cfgmodulo_delete_match']);
+    $routes->get('400/(:any)', 'Config\\CfgModulo::ativinativ/$1', ['as' => 'cfgmodulo_ativinativ_match']);
+});
 
-$routes->get('/CriaEtiqueta', 'CriaEtiqueta::emiteEtiqueta');
-$routes->get('/CriaEtiqueta/emiteEtiqueta/(:any)', 'CriaEtiqueta::emiteEtiqueta/$1');
-$routes->post('/CriaEtiqueta/emiteEtiqueta/(:any)', 'CriaEtiqueta::emiteEtiqueta/$1');
-$routes->get('/CriaEtiqueta/previewEtiquetaViaAjax/(:any)', 'CriaEtiqueta::previewEtiquetaViaAjax/$1');
-$routes->post('/CriaEtiqueta/previewEtiquetaViaAjax/(:any)', 'CriaEtiqueta::previewEtiquetaViaAjax/$1');
+// Controladores de Estoque
+$estoqueControllers = [
+    'SaldoEstoque', 'Movimento', 'Deposito', 'Transacao',
+    'TipoMovimentacao', 'Requisicao', 'CfgEtiqueta',
+    'AteRequisicao', 'ConfRequisicao'
+];
 
-$routes->get('/CriaEtiquetaZPL', 'CriaEtiquetaZPL::emiteEtiqueta');
-$routes->get('/CriaEtiquetaZPL/emiteEtiqueta/(:any)', 'CriaEtiquetaZPL::emiteEtiqueta/$1');
-$routes->post('/CriaEtiquetaZPL/emiteEtiqueta/(:any)', 'CriaEtiquetaZPL::emiteEtiqueta/$1');
-$routes->get('/CriaEtiquetaZPL/emiteEtiqueta/(:any)/(:any)', 'CriaEtiquetaZPL::emiteEtiqueta/$1/$2');
-$routes->post('/CriaEtiquetaZPL/emiteEtiqueta/(:any)/(:any)', 'CriaEtiquetaZPL::emiteEtiqueta/$1/$2');
-$routes->get('/CriaEtiquetaZPL/previewEtiquetaViaAjax/(:any)', 'CriaEtiquetaZPL::previewZPL/$1');
-$routes->post('/CriaEtiquetaZPL/previewEtiquetaViaAjax/(:any)', 'CriaEtiquetaZPL::previewZPL/$1');
+foreach ($estoqueControllers as $ctrl) {
+    $routes->group($ctrl, static function ($routes) use ($ctrl) {
+        $name = strtolower($ctrl);
+        $routes->get('/', "Estoque\\$ctrl::index", ['as' => "{$name}_index"]);
+        $routes->match(['get', 'post'], '(:any)', "Estoque\\$ctrl::$1", ['as' => "{$name}_match"]);
+    });
+}
 
-$routes->get('/CfgCor', 'Config\CfgCor::index');
-$routes->get('/CfgCor/(:any)', 'Config\CfgCor::$1');
-$routes->post('/CfgCor/(:any)', 'Config\CfgCor::$1');
+$routes->get('AteRequisicao/GeraEtiqueta/(:num)/(:num)', 'Estoque\\AteRequisicao::GeraEtiqueta/$1/$2', ['as' => 'aterequisicao_GeraEtiqueta_match']);
 
-$routes->get('/CfgModulo', 'Config\CfgModulo::index');
-$routes->get('/CfgModulo/000', 'Config\CfgModulo::add');
-$routes->get('/CfgModulo/100/(:any)', 'Config\CfgModulo::show/$1');
-$routes->get('/CfgModulo/200/(:any)', 'Config\CfgModulo::edit/$1');
-$routes->get('/CfgModulo/300/(:any)', 'Config\CfgModulo::delete/$1');
-$routes->get('/CfgModulo/400/(:any)', 'Config\CfgModulo::ativinativ/$1');
-$routes->get('/CfgModulo/(:any)', 'Config\CfgModulo::$1');
-$routes->post('/CfgModulo/(:any)', 'Config\CfgModulo::$1');
+// Controladores de Produto
+$produtoControllers = [
+    'Origem', 'Familia', 'Lote', 'ProClasse',
+    'ProIngrediente', 'Produto', 'Fabricante'
+];
 
-$routes->get('/CfgTela', 'Config\CfgTela::index');
-$routes->get('/CfgTela/(:any)', 'Config\CfgTela::$1');
-$routes->post('/CfgTela/(:any)', 'Config\CfgTela::$1');
+foreach ($produtoControllers as $ctrl) {
+    $routes->group($ctrl, static function ($routes) use ($ctrl) {
+        $name = strtolower($ctrl);
+        $routes->get('/', "Produto\\$ctrl::index", ['as' => "{$name}_index"]);
+        $routes->match(['get', 'post'], '(:any)', "Produto\\$ctrl::$1", ['as' => "{$name}_match"]);
+    });
+}
 
-$routes->get('/CfgMenu', 'Config\CfgMenu::index');
-$routes->get('/CfgMenu/(:any)', 'Config\CfgMenu::$1');
-$routes->post('/CfgMenu/(:any)', 'Config\CfgMenu::$1');
+// Grupo: Micro
+$routes->group('Analise', static function ($routes) {
+    $routes->get('/', 'Micro\\Analise::index', ['as' => 'analise_index']);
+    $routes->match(['get', 'post'], '(:any)', 'Micro\\Analise::$1', ['as' => 'analise_match']);
+});
 
-$routes->get('/CfgDicionario', 'Config\CfgDicionario::index');
-$routes->get('/CfgDicionario/000/(:any)', 'Config\CfgDicionario::show/$1');
-$routes->get('/CfgDicionario/(:any)', 'Config\CfgDicionario::$1');
-$routes->post('/CfgDicionario/(:any)', 'Config\CfgDicionario::$1');
+$routes->group('AnaRequisicao', static function ($routes) {
+    $routes->get('/', 'Micro\\AnaRequisicao::index', ['as' => 'anarequisicao_index']);
+    $routes->match(['get', 'post'], '(:any)', 'Micro\\AnaRequisicao::$1', ['as' => 'anarequisicao_match']);
+});
 
-$routes->get('/CfgFuncoes', 'Config\CfgFuncoes::index');
-$routes->get('/CfgFuncoes/(:any)', 'Config\CfgFuncoes::$1');
-$routes->post('/CfgFuncoes/(:any)', 'Config\CfgFuncoes::$1');
+// Grupo: Ocorrencia
+$routes->group('OcoTipoAcao', static function ($routes) {
+    $routes->get('/', 'Ocorrencia\\OcoTipoAcao::index', ['as' => 'ocotipoacao_index']);
+    $routes->match(['get', 'post'], '(:any)', 'Ocorrencia\\OcoTipoAcao::$1', ['as' => 'ocotipoacao_match']);
+});
 
-$routes->get('/CfgPerfil', 'Config\CfgPerfil::index');
-$routes->get('/CfgPerfil/(:any)', 'Config\CfgPerfil::$1');
-$routes->post('/CfgPerfil/(:any)', 'Config\CfgPerfil::$1');
+$routes->group('OcoTipoOcorrencia', static function ($routes) {
+    $routes->get('/', 'Ocorrencia\\OcoTipoOcorrencia::index', ['as' => 'ocotipoocorrencia_index']);
+    $routes->match(['get', 'post'], '(:any)', 'Ocorrencia\\OcoTipoOcorrencia::$1', ['as' => 'ocotipoocorrencia_match']);
+});
 
-$routes->get('/CfgUsuario', 'Config\CfgUsuario::index');
-$routes->get('/CfgUsuario/(:any)', 'Config\CfgUsuario::$1');
-$routes->post('/CfgUsuario/(:any)', 'Config\CfgUsuario::$1');
+// Grupo: WebService
+$routes->group('WsCeqweb', static function ($routes) {
+    $routes->match(['get', 'post'], '(:any)', 'Ws\\WsCeqweb::$1', ['as' => 'wsceqweb_single_match']);
+    $routes->match(['get', 'post'], '(:any)/(:any)', 'Ws\\WsCeqweb::$1::$2', ['as' => 'wsceqweb_double_match']);
+    $routes->match(['get', 'post'], '(:any)/(:any)/(:any)', 'Ws\\WsCeqweb::$1::$2::$3', ['as' => 'wsceqweb_triple_match']);
+});
 
-$routes->get('/CfgMensagem', 'Config\CfgMensagem::index');
-$routes->get('/CfgMensagem/(:any)', 'Config\CfgMensagem::$1');
-$routes->post('/CfgMensagem/(:any)', 'Config\CfgMensagem::$1');
-
-$routes->get('/CfgStatus', 'Config\CfgStatus::index');
-$routes->get('/CfgStatus/(:any)', 'Config\CfgStatus::$1');
-$routes->post('/CfgStatus/(:any)', 'Config\CfgStatus::$1');
-
-$routes->get('/CfgLayoutEtiq', 'Config\CfgLayoutEtiq::index');
-$routes->get('/CfgLayoutEtiq/(:any)', 'Config\CfgLayoutEtiq::$1');
-$routes->post('/CfgLayoutEtiq/(:any)', 'Config\CfgLayoutEtiq::$1');
-
-$routes->get('/CfgEtiqueta', 'Config\CfgEtiqueta::index');
-$routes->get('/CfgEtiqueta/(:any)', 'Config\CfgEtiqueta::$1');
-$routes->post('/CfgEtiqueta/(:any)', 'Config\CfgEtiqueta::$1');
-
-$routes->get('/CfgImpressora', 'Config\CfgImpressora::index');
-$routes->get('/CfgImpressora/(:any)', 'Config\CfgImpressora::$1');
-$routes->post('/CfgImpressora/(:any)', 'Config\CfgImpressora::$1');
-
-$routes->get('/CfgEmpresa', 'Config\CfgEmpresa::index');
-$routes->get('/CfgEmpresa/(:any)', 'Config\CfgEmpresa::$1');
-$routes->post('/CfgEmpresa/(:any)', 'Config\CfgEmpresa::$1');
-
-// $routes->match(['get', 'post'], '/CfgModulo/(:any)/(:any)', 'Config\CfgModulo::$1::$2');
-
-$routes->get('/SaldoEstoque', 'Estoque\SaldoEstoque::index');
-$routes->get('/SaldoEstoque/(:any)', 'Estoque\SaldoEstoque::$1');
-$routes->post('/SaldoEstoque/(:any)', 'Estoque\SaldoEstoque::$1');
-
-$routes->get('/Movimento', 'Estoque\Movimento::index');
-$routes->get('/Movimento/(:any)', 'Estoque\Movimento::$1');
-$routes->post('/Movimento/(:any)', 'Estoque\Movimento::$1');
-
-
-$routes->get('/Deposito', 'Estoque\Deposito::index');
-$routes->get('/Deposito/(:any)', 'Estoque\Deposito::$1');
-$routes->post('/Deposito/(:any)', 'Estoque\Deposito::$1');
-
-$routes->get('/Transacao', 'Estoque\Transacao::index');
-$routes->get('/Transacao/(:any)', 'Estoque\Transacao::$1');
-$routes->post('/Transacao/(:any)', 'Estoque\Transacao::$1');
-
-$routes->get('/TipoMovimentacao', 'Estoque\TipoMovimentacao::index');
-$routes->get('/TipoMovimentacao/(:any)', 'Estoque\TipoMovimentacao::$1');
-$routes->post('/TipoMovimentacao/(:any)', 'Estoque\TipoMovimentacao::$1');
-
-$routes->get('/Requisicao', 'Estoque\Requisicao::index');
-$routes->get('/Requisicao/(:any)', 'Estoque\Requisicao::$1');
-$routes->post('/Requisicao/(:any)', 'Estoque\Requisicao::$1');
-
-$routes->get('/AteRequisicao', 'Estoque\AteRequisicao::index');
-$routes->get('/AteRequisicao/(:any)', 'Estoque\AteRequisicao::$1');
-$routes->post('/AteRequisicao/(:any)', 'Estoque\AteRequisicao::$1');
-$routes->get('/AteRequisicao/GeraEtiqueta/(:num)/(:num)', 'Estoque\AteRequisicao::GeraEtiqueta/$1/$2');
-
-$routes->get('/ConfRequisicao', 'Estoque\ConfRequisicao::index');
-$routes->get('/ConfRequisicao/(:any)', 'Estoque\ConfRequisicao::$1');
-$routes->post('/ConfRequisicao/(:any)', 'Estoque\ConfRequisicao::$1');
-
-
-$routes->get('/Origem', 'Produto\Origem::index');
-$routes->get('/Origem/(:any)', 'Produto\Origem::$1');
-$routes->post('/Origem/(:any)', 'Produto\Origem::$1');
-
-$routes->get('/Familia', 'Produto\Familia::index');
-$routes->get('/Familia/(:any)', 'Produto\Familia::$1');
-$routes->post('/Familia/(:any)', 'Produto\Familia::$1');
-
-$routes->get('/Lote', 'Produto\Lote::index');
-$routes->get('/Lote/(:any)', 'Produto\Lote::$1');
-$routes->post('/Lote/(:any)', 'Produto\Lote::$1');
-
-$routes->get('/ProClasse', 'Produto\ProClasse::index');
-$routes->get('/ProClasse/(:any)', 'Produto\ProClasse::$1');
-$routes->post('/ProClasse/(:any)', 'Produto\ProClasse::$1');
-
-$routes->get('/ProIngrediente', 'Produto\ProIngrediente::index');
-$routes->get('/ProIngrediente/(:any)', 'Produto\ProIngrediente::$1');
-$routes->post('/ProIngrediente/(:any)', 'Produto\ProIngrediente::$1');
-
-$routes->get('/Produto', 'Produto\Produto::index');
-$routes->get('/Produto/(:any)', 'Produto\Produto::$1');
-$routes->post('/Produto/(:any)', 'Produto\Produto::$1');
-
-$routes->get('/Fabricante', 'Produto\Fabricante::index');
-$routes->get('/Fabricante/(:any)', 'Produto\Fabricante::$1');
-$routes->post('/Fabricante/(:any)', 'Produto\Fabricante::$1');
-
-$routes->get('/Analise', 'Micro\Analise::index');
-$routes->get('/Analise/(:any)', 'Micro\Analise::$1');
-$routes->post('/Analise/(:any)', 'Micro\Analise::$1');
-
-$routes->get('/AnaRequisicao', 'Micro\AnaRequisicao::index');
-$routes->get('/AnaRequisicao/(:any)', 'Micro\AnaRequisicao::$1');
-$routes->post('/AnaRequisicao/(:any)', 'Micro\AnaRequisicao::$1');
-
-$routes->get('/OcoTipoAcao', 'Ocorrencia\OcoTipoAcao::index');
-$routes->get('/OcoTipoAcao/(:any)', 'Ocorrencia\OcoTipoAcao::$1');
-$routes->post('/OcoTipoAcao/(:any)', 'Ocorrencia\OcoTipoAcao::$1');
-
-$routes->get('/OcoTipoOcorrencia', 'Ocorrencia\OcoTipoOcorrencia::index');
-$routes->get('/OcoTipoOcorrencia/(:any)', 'Ocorrencia\OcoTipoOcorrencia::$1');
-$routes->post('/OcoTipoOcorrencia/(:any)', 'Ocorrencia\OcoTipoOcorrencia::$1');
-
-$routes->match(['GET', 'POST'], '/WsCeqweb/(:any)', 'Ws\WsCeqweb::$1');
-$routes->match(['GET', 'POST'], '/WsCeqweb/(:any)/(:any)', 'Ws\WsCeqweb::$1::2');
-$routes->match(['GET', 'POST'], '/WsCeqweb/(:any)/(:any)/(:any)', 'Ws\WsCeqweb::$1::2::3');
-
-
-$routes->get('teste-handler', 'Home::testarEmailHandler');
-$routes->get('teste-email', 'Home::testeEmailManual');
-
-/*
- * --------------------------------------------------------------------
- * Additional Routing
- * --------------------------------------------------------------------
- *
- * There will often be times that you need additional routing and you
- * need it to be able to override any defaults in this file. Environment
- * based routes is one such time. require() additional route files here
- * to make that happen.
- *
- * You will have access to the $routes object within that file without
- * needing to reload it.
- */
+// Rotas por ambiente
 if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
     require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
