bf3ebea58a38e94d3ce56e147949d926e5ca0f67
Douglas Ferreira <douglas@4web.net.br>
2025-10-07 23:50:12 -0300

    Atualizacao 07/10/2025 23:50:11

--- app/Views/strut/vw_rodape.php
+++ app/Views/strut/vw_rodape.php
@@ -15,10 +15,46 @@
     <div class="text-end d-inline position-absolute pe-5" style="right:1rem"><?= $identificador; ?></div>
   <?
   } ?>
-  <div class="text-end d-inline position-absolute pe-5" style="right:0rem">
-    <div id="stat_server" class="spinner-grow spinner-grow-sm " role="status" title="Servidor Conectado" onclick="executa_php()"></div>
-  </div>
+  <!-- <div class="text-end d-inline position-absolute bg-info" style="right:10rem"> -->
+    <!-- <div id="stat_server" class="spinner-grow spinner-grow-sm " role="status" title="Servidor Conectado" onclick="executa_php()"></div> -->
+    <div id="stat_server" class="float-end truck-icon" role="status" title="Servidor Conectado" onclick="executa_php()">
+      <i class="fa-solid fa-truck-moving"></i>
+    </div>
+  <!-- </div> -->
 </div>
+<style>
+  .truck-icon {
+    position: absolute;
+    display: inline-block;
+    width: 60px; /* garante o espaço da animação */
+    right: 5rem;
+    font-size: 12px;
+    /* overflow: hidden; */
+    cursor: pointer;
+  }
+
+  .truck-icon i {
+    color: green;
+    font-size: 1em;
+    position: relative;
+    animation: moverCaminhao 3s infinite ease-in-out;
+  }
+
+  @keyframes moverCaminhao {
+    0% {
+      transform: translateX(0) scaleX(1);/* normal */
+    }
+    50% {
+      transform: translateX(60px) scaleX(1); /* indo para direita */
+    }
+    51% {
+      transform: translateX(60px) scaleX(-1); /* começa a voltar */
+    }
+    100% {
+      transform: translateX(0) scaleX(-1); /* voltando para a esquerda */
+    }
+  }
+  </style>
 <script>
   function executa_php() {
     redirec_blank('/Utils/executa_php');
